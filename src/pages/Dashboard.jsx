023148f1f02c07f542955d7ebe609840bb7b71eb
Ha-no <gksdn1216@naver.com>
2023-06-04 19:35:40 +0900

    [UPDATE] Skeleton
    사용 페이지 Routing

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -19,30 +19,25 @@ import DashboardCard10 from '../partials/dashboard/DashboardCard10';
 import DashboardCard11 from '../partials/dashboard/DashboardCard11';
 
 function Dashboard() {
-
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <div className="flex h-screen overflow-hidden">
-
       {/* Sidebar */}
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
       {/* Content area */}
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-
         {/*  Site header */}
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
         <main>
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-
             {/* Welcome banner */}
             <WelcomeBanner />
 
             {/* Dashboard actions */}
             <div className="sm:flex sm:justify-between sm:items-center mb-8">
-
               {/* Left: Avatars */}
               <DashboardAvatars />
 
@@ -54,18 +49,19 @@ function Dashboard() {
                 <Datepicker align="right" />
                 {/* Add view button */}
                 <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white">
-                  <svg className="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                  <svg
+                    className="w-4 h-4 fill-current opacity-50 shrink-0"
+                    viewBox="0 0 16 16"
+                  >
                     <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
                   </svg>
                   <span className="hidden xs:block ml-2">Add View</span>
-                </button>                
+                </button>
               </div>
-
             </div>
 
             {/* Cards */}
             <div className="grid grid-cols-12 gap-6">
-
               {/* Line chart (Acme Plus) */}
               <DashboardCard01 />
               {/* Line chart (Acme Advanced) */}
@@ -88,14 +84,10 @@ function Dashboard() {
               <DashboardCard10 />
               {/* Card (Income/Expenses) */}
               <DashboardCard11 />
-              
             </div>
-
           </div>
         </main>
-
       </div>
-
     </div>
   );
 }
---
+++ src/pages/plant/index.jsx
@@ -0,0 +1,15 @@
+import PlantMain from './PlantMain';
+import PlantRegister from './PlantRegister';
+import PlantDetail from './PlantDetail';
+import PlantComplaints from './PlantComplaints';
+import PlantReview from './PlantReview';
+
+const plant = {
+  PlantMain,
+  PlantRegister,
+  PlantDetail,
+  PlantComplaints,
+  PlantReview,
+};
+
+export default plant;
